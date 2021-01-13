f73a644a902f220f4ac92421954b440a2a609f61
Zach Hornung <Zachhornung@comcast.net>
2021-01-12 16:51:28 -0800

    added different files for css and javascript

---
+++ app.js
@@ -0,0 +1,59 @@
+'use strict';
+var userName = prompt('Hello, welcome to my website! What is your name?')
+  alert('Welcome, ' + userName + '! Glad you stopped by!')
+
+var workingInRain = prompt('Do I like working in the rain, yes or no?').toUpperCase()
+if (workingInRain == 'YES' || workingInRain == 'Y'){
+  // console.log('Sorry, that is not true')
+  alert('Sorry, that is not true');
+} else if (workingInRain == 'NO' || workingInRain == 'N'){
+  // console.log('That\'s correct!')
+  alert('That\'s correct!');
+}
+
+var school = prompt('Did I attend Western Washington University, yes or no?').toUpperCase()
+if (school == 'YES' || school == 'Y'){
+  // console.log('That\'s correct!')
+  alert('That\'s correct!');
+} else if (school == 'NO' || school == 'N'){
+  // console.log('Sorry, that is not true')
+  alert('Sorry, that is not true');
+}
+
+var bike = prompt('Did I Ride the Divide, yes or no?').toUpperCase()
+if (bike == 'YES' || bike == 'Y'){
+  // console.log('That\'s correct!')
+  alert('That\'s correct!');
+} else if (bike == 'NO' || bike == 'N'){
+  // console.log('Sorry, that is not true')
+  alert('Sorry, that is not true');
+}
+
+var oyster = prompt('Did I farm oysters, yes or no?').toUpperCase()
+if (oyster == 'YES' || oyster == 'Y'){
+  // console.log('That\'s correct!')
+  alert('That\'s correct!');
+} else if (oyster == 'NO' || oyster == 'N'){
+  // console.log('Sorry, that is not true')
+  alert('Sorry, that is not true');
+}
+
+var oyster = prompt('Did I farm oysters, yes or no?').toUpperCase()
+if (oyster == 'YES' || oyster == 'Y'){
+  // console.log('That\'s correct!')
+  alert('That\'s correct!');
+} else if (oyster == 'NO' || oyster == 'N'){
+  // console.log('Sorry, that is not true')
+  alert('Sorry, that is not true');
+}
+
+var ai = prompt('Did I want to get into the AI field, yes or no?').toUpperCase()
+if (ai == 'YES' || ai == 'Y'){
+  // console.log('That\'s correct!')
+  alert('That\'s correct!');
+} else if (ai == 'NO' || ai == 'N'){
+  // console.log('Sorry, that is not true')
+  alert('Sorry, that is not true');
+}
+
+alert('Thanks again for stopping by, ' + userName + '! Hope to see you again soon!');
