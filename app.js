d5227729e6249f77f6400d41b5c3c43b5d839b54
rjsweet <roger.j.sweet@gmail.com>
2021-01-14 18:54:20 -0800

    wrapped js snippets in functions

--- app.js
+++ app.js
@@ -2,46 +2,66 @@
 var userName = prompt('Hello, welcome to my website! What is your name?')
   alert('Welcome, ' + userName + '! Glad you stopped by!')
 var ca = 0
-var workingInRain = prompt('Do I like working in the rain, yes or no?').toUpperCase()
+
+function workingInRain(){
+  var workingInRain = prompt('Do I like working in the rain, yes or no?').toUpperCase()
 if (workingInRain == 'YES' || workingInRain == 'Y'){
   // console.log('Sorry, that is not true')
   alert('Sorry, that is not true');
 } else if (workingInRain == 'NO' || workingInRain == 'N'){
   // console.log('That\'s correct!')
   alert('That\'s correct!');
-  ca++
+  ca++;
+  return ca;
   // console.log(ca)
 }
 
-var school = prompt('Did I attend Western Washington University, yes or no?').toUpperCase()
+}
+workingInRain()
+
+
+function school(){
+  var school = prompt('Did I attend Western Washington University, yes or no?').toUpperCase()
 if (school == 'YES' || school == 'Y'){
   // console.log('That\'s correct!')
   alert('That\'s correct!');
   ca++;
+  return ca;
 } else if (school == 'NO' || school == 'N'){
   // console.log('Sorry, that is not true')
   alert('Sorry, that is not true');
 }
+}
+school();
 
-var bike = prompt('Did I Ride the Divide, yes or no?').toUpperCase()
+function bike(){
+  var bike = prompt('Did I Ride the Divide, yes or no?').toUpperCase()
 if (bike == 'YES' || bike == 'Y'){
   // console.log('That\'s correct!')
   alert('That\'s correct!');
   ca++;
+  return ca;
 } else if (bike == 'NO' || bike == 'N'){
   // console.log('Sorry, that is not true')
   alert('Sorry, that is not true');
 }
+}
+bike();
 
-var oyster = prompt('Did I farm oysters, yes or no?').toUpperCase()
+
+function oyster(){
+  var oyster = prompt('Did I farm oysters, yes or no?').toUpperCase()
 if (oyster == 'YES' || oyster == 'Y'){
   // console.log('That\'s correct!')
   alert('That\'s correct!');
   ca++;
+  return ca;
 } else if (oyster == 'NO' || oyster == 'N'){
   // console.log('Sorry, that is not true')
   alert('Sorry, that is not true');
 }
+}
+oyster();
 
 // var oyster = prompt('Did I farm oysters, yes or no?').toUpperCase()
 // if (oyster == 'YES' || oyster == 'Y'){
@@ -53,15 +73,20 @@ if (oyster == 'YES' || oyster == 'Y'){
 //   alert('Sorry, that is not true');
 // }
 
-var ai = prompt('Did I want to get into the AI field, yes or no?').toUpperCase()
+
+function ai(){
+  var ai = prompt('Did I want to get into the AI field, yes or no?').toUpperCase()
 if (ai == 'YES' || ai == 'Y'){
   // console.log('That\'s correct!')
   alert('That\'s correct!');
-  ca++
+  ca++;
+  return ca;
 } else if (ai == 'NO' || ai == 'N'){
   // console.log('Sorry, that is not true')
   alert('Sorry, that is not true');
 }
+}
+ai();
 
 // for (var i = 0; i <=3; i++){
 // let userInput = prompt('Guess a number between 1 and 100')
@@ -79,7 +104,9 @@ if (ai == 'YES' || ai == 'Y'){
     
 //   }
 // }
-let answer = 51
+
+function numberGame(){
+  let answer = 51
 
 for ( var i = 0; i <=3; i++){
  var userInput = prompt('Guess a number between 1 and 100')
@@ -89,11 +116,15 @@ for ( var i = 0; i <=3; i++){
     alert('Too High! Try again')
   } else if (userInput == answer){
     alert('Great job, thats the right answer!');
-    ca++; break;
+    ca++; 
+    break;
   }
+}return ca;
 }
+numberGame();
 
-let arrayOfAnswers = ['meshuggah', 'opeth', 'nick  drake', 'ahab', 'bathory', 'behemoth', 'bolt thrower', 'the contortionist', 'dead can dance', 'emperor', 'ennio morricone']
+function bandGame(){
+  let arrayOfAnswers = ['meshuggah', 'opeth', 'nick  drake', 'ahab', 'bathory', 'behemoth', 'bolt thrower', 'the contortionist', 'dead can dance', 'emperor', 'ennio morricone']
 outerloop:
 for (let i = 6; i >= 1; i--){
   var bandAnswer = prompt('What is one of my favorite bands? You have ' + i + ' guesses ramaining')
@@ -106,7 +137,9 @@ for (let i = 6; i >= 1; i--){
   }
   if (i == 1){
     alert('thats your last try! the correct answers were meshuggah, opeth, nick drake, ahab, bathory, behemoth, bolt thrower, the contortionist, dead can dance, emperor, and ennio morricone.')
-  }
+  }return ca;
 }
+}
+bandGame();
 alert('You got ' + ca + ' answers correct!')
 alert('Thanks again for stopping by, ' + userName + '! Hope to see you again soon!');
